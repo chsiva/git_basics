0418d4e2aaffbf63d540bbaf298a5e0ebf62aaf6
chsiva <36084965+chsiva@users.noreply.github.com>
2025-06-28 12:13:46 -0400

    Delete .github/workflows/main.tf

--- .github/workflows/main.tf
+++
@@ -1,9 +0,0 @@
-resource "null_resource" "get_hostname" {
-  provisioner "local-exec" {
-    command = "hostname"  # Command to get the hostname
-  }
-}
-
-output "local_hostname" {
-  value = resource.null_resource.get_hostname.triggers["command_output"]
-}
